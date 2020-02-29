439e2619ce7a92a08826992ffe0b58419b024a99
Syed Fazil Basheer <fazil.basheer@kuvera.in>
2020-02-29 10:36:33 +0530

    validate skill_body on join

--- app/models/learning/skills_knowledge_concept.rb
+++ app/models/learning/skills_knowledge_concept.rb
@@ -3,6 +3,8 @@ class Learning::SkillsKnowledgeConcept < ApplicationRecord
   belongs_to :skill
   belongs_to :knowledge_concept, class_name: 'Knowledge::Concept'
 
+  validate :skill_body_should_be_valid
+
   def skill_body
     skill&.body
   end
@@ -10,5 +12,10 @@ class Learning::SkillsKnowledgeConcept < ApplicationRecord
   def skill_body=(new_body)
     self.skill = Learning::Skill.create_or_find_by(body: new_body)
   end
+
+  private
+
+  def skill_body_should_be_valid
+    skill.errors[:body].each { |e| errors[:skill_body] << e }
   end
 end
