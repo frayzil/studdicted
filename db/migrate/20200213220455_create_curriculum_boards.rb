eef3efe407d6233d075419b81fccc3bb1cedf833
Syed Fazil Basheer <fazil.basheer@kuvera.in>
2020-03-03 10:44:11 +0530

    add unique index to board name

--- db/migrate/20200213220455_create_curriculum_boards.rb
+++ db/migrate/20200213220455_create_curriculum_boards.rb
@@ -1,7 +1,7 @@
 class CreateCurriculumBoards < ActiveRecord::Migration[6.0]
   def change
     create_table :curriculum_boards do |t|
-      t.string :name
+      t.string :name, index: { unique: true }
 
       t.timestamps
     end
